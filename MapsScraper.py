d84408fc9770b69c248fec3948efbd9a6fac11af
zaiwte <d38saac@hotmail.com>
2023-08-20 01:18:54 -0400

    funcion open_drive agregada, barra de progreso agregada, evento de boton carga modificado.

--- MapsScraper.py
+++ MapsScraper.py
@@ -6,6 +6,7 @@ import time
 import PySimpleGUI as sg
 import re
 import random
+import threading
 """
 driver.execute_script("window.scrollBy(0,document.body.scrollHeight);",)
 print(pague.prettify())
@@ -24,41 +25,59 @@ print(len(list(block2)))
 class Map:
     def __init__(self):
         # url = "https://www.google.es/maps/search/clinica+dental+madrid+28001/@40.4193747,-3.6982363,16z?entry=ttu"
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
-        self.patron='http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-        time.sleep(1)
-
+        self.patron = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    def open_driver(self):
+        try:
+            self.driver = webdriver.Chrome()
+            self.driver.maximize_window()
+        except:
+            print("error open driver")
     def search(self, text):
         try:
             self.driver.get(text)
+
         except:
-            print("error")
+            print('error search')
 
     def get_info(self):
         pague = BeautifulSoup(self.driver.page_source, 'html.parser')
-        n=3
-        for i in pague.find_all('a', attrs={'class': 'hfpxzc'}):
-            urls = re.findall(self.patron, str(i))
-            self.search(urls[0])
-            print("-----------------------------------------------")
-            time.sleep(random.randint(3, 7))
-        #print(len(list(block)))
-        time.sleep(5)
+        block = pague.find_all('a', attrs={'class': 'hfpxzc'})
+        return block
+        #self.search(urls[0])
+        #print(urls[0])
+        #time.sleep(random.randint(3, 7))
+
+        #time.sleep(5)
 
     def close_driver(self):
-        self.driver.close()
+        try:
+            self.driver.close()
+        except:
+            print('error close driver')
 
 
 class MapsScraperGUI:
     def __init__(self):
         self.map = Map()
-        self.map.search("https://www.google.com")
+        self.map.open_driver()
+        self.values = [['', '', '', '', '', '']]
+        self.headings = ['nombre', 'reviews', 'estrellas', 'ubicacion', 'telefono', 'web']
 
     def show(self):
         layout = [
-                    [sg.Input(key='-INPUT-'), sg.Button("search", key='-SEARCH-')],
-                    [sg.Button("load", key='-LOAD-',auto_size_button=True)]
+                    [sg.Input(key='-INPUT-'), sg.Button("buscar", key='-SEARCH-'), sg.Button("abrir navegador", key='-OPENDRIVER-')],
+                    [sg.Text('resultados:'), sg.Input(size=(8,0), key='-RESULTS-')],
+                    [sg.Table(values=self.values,
+                                headings=self.headings,
+                                auto_size_columns=False,
+                                justification='left',
+                                def_col_width=12,
+                                num_rows=12,
+                                key='-TABLA_INI-',
+                                font=['20'],
+                                vertical_scroll_only=False)],
+                    [sg.ProgressBar(max_value=100,orientation='h',expand_x=True, key='-PBAR-', size=(20,15))],
+                    [sg.Button("cargar", key='-LOAD-', size=(20,0))]
                  ]
 
         window = sg.Window('MapsScraper',
@@ -70,15 +89,33 @@ class MapsScraperGUI:
 
             if event == '-SEARCH-':
                 self.map.search("https://www.google.es/maps/search/" + values['-INPUT-'].replace(" ", "+"))
+                window.bring_to_front()
+
+            if event == '-OPENDRIVER-':
+                self.map.open_driver()
+                window.bring_to_front()
 
             if event == '-LOAD-':
-                self.map.get_info()
+                window['-LOAD-'].update(disabled=True)
+                infoMap = self.map.get_info()
+                lenInfoMap = len(infoMap)
+                window['-RESULTS-'].update(str(lenInfoMap))
+                n=0
+                for i in infoMap:
+                    n+=1
+                    urls = re.findall(self.map.patron, str(i))
+                    print(urls)
+                    window['-PBAR-'].update(current_count=round(n*(100/lenInfoMap)))
+                    print(f"current count: {round(n*(100/lenInfoMap))}")
+
+                window['-LOAD-'].update(disabled=False)
 
             if event in (sg.WIN_CLOSED, 'Exit'):
+                self.map.close_driver()
                 break
 
         window.close()
-        self.map.close_driver()
+
 
 
 if __name__ == '__main__':
